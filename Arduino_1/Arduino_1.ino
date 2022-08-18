9767fbd2645bd97b0a664930bc7af215d15ef21b
vamshi-poreddy <80277493+vamshi-poreddy@users.noreply.github.com>
2022-08-18 23:31:14 +0100

    Add files via upload

--- Arduino_1/Arduino_1.ino
+++ Arduino_1/Arduino_1.ino
@@ -7,21 +7,21 @@
 
 WiFiClient wifiClient;
 MqttClient mqttClient(wifiClient);
-BLEService pirService("19B10000-E8F2-537E-4F6C-D104768A1214"); // create service
-BLEByteCharacteristic pirCharacteristic("19B10001-E8F2-537E-4F6C-D104768A1214", BLERead | BLENotify);
+BLEService pirService("19B10000-E8F2-537E-4F6C-D104768A1214"); // creating an LED service
+BLEByteCharacteristic pirCharacteristic("19B10001-E8F2-537E-4F6C-D104768A1214", BLERead | BLENotify); // PIR charasteristic with read and notify attributes
 
 ///////please enter your sensitive data in the Secret tab/arduino_secrets.h
 char ssid[] = SECRET_SSID;        // your network SSID (name)
 char pass[] = SECRET_PASS;    // your network password (use for WPA, or use as key for WEP)
 int status = WL_IDLE_STATUS;     // the WiFi radio's status
-const int led_pin = 11;
+const int led_pin = 11; // Device Led Pin
 const int PIn = 2;     // the number of the input pin
 int pirState = 0;
-const char broker[] = "10.13.122.148"; // Address of the MQTT server
-const int  port     = 1883;
+const char broker[] = "10.13.122.148"; // Address of the MQTT broker
+const int  port     = 1883;   //port for MQTT connection
 //const int serverPort = 4080;
-const char topic[] = "ToArduino/pir";
-const char topic_pir[] = "FromArduino/pir";
+const char topic[] = "ToArduino/pir";   // Topic to control led pin from raspberry pi
+const char topic_pir[] = "FromArduino/pir";   //Topic for communication with second arduino
 String msg = "";
 int count =0; 
 int ble_status=0;
@@ -34,100 +34,108 @@ int timer;
 void setup() {
   //Initialize serial and wait for port to open:
   Serial.begin(9600);
-  //while (!Serial) {
-    ; // wait for serial port to connect. Needed for native USB port only
-  //}
-  wifi_ble();
-  pinMode(led_pin, OUTPUT);
+  /*while (!Serial) {
+    ; wait for serial port to connect. Needed for native USB port only
+  //}*/
+  pinMode(LED_BUILTIN, OUTPUT);
+  wifi_ble();//function for starting up Wifi or BLE
+  pinMode(led_pin, OUTPUT);//Setting LED pin mode to output
   digitalWrite(led_pin, LOW);
-  pinMode(PIn, INPUT);
+  pinMode(PIn, INPUT);//Setting pin mode with PIR to input
 }
 
 void loop() {
-  pirState = digitalRead(PIn);
+  pirState = digitalRead(PIn); //Read the pir state
   if(pirState == HIGH){
-    digitalWrite(led_pin, HIGH);
+    digitalWrite(led_pin, HIGH);//Writes to LED device based on pir state
     timer =1;
   }
   if(timer){
     unsigned long currentMillis = millis();
     if (currentMillis - previousMillis >= interval) {
     previousMillis = currentMillis;
-    digitalWrite(led_pin, LOW);
+    digitalWrite(led_pin, LOW);//Swithching of LED device after 10 sec interval if was switched on from PIR state
   }
   }
   //Serial.println(mqttClient.connected());
   if(mqttClient.connected()){
-    mqtt_func();
+    mqtt_func();//if MQTT client is conneceted calling the function for publishing and subscribing
   }
   else{
-    if(!ble_status){
-      WiFi.disconnect();
+    if(!ble_status){ //if mqtt client is not connected and BLE is not switched on calls the below fucntion to set up after a 5 second delay
+      delay(5000);
+      WiFi.disconnect();//disconnecting the wifi
       wifi_ble();
     }
+    if(ble_status){
+    //once BLE is switched on calling the poll for keeping the connection
     BLE.poll();
     BLEDevice central = BLE.central();
     if(central.connected()){
-      pirCharacteristic.writeValue(pirState);
+      pirCharacteristic.writeValue(pirState); //updates the characteristics value when the device is connected
     }
    // unsigned long currentMillis = millis();
     //if (currentMillis - previousMillis1 >= mqtt_check){
      // previousMillis1 = currentMillis;
       //wifi_ble();
     //}
+    }
   }
 }
 
 void mqtt_func(){
-  mqttClient.poll();
+  mqttClient.poll(); //To keep the MQTT connection alive
   if (pirState == HIGH) {
     bool retained = false;
     int qos = 1;
     bool dup = false;
-    mqttClient.beginMessage(topic_pir, pirState, retained, qos, dup);
+    mqttClient.beginMessage(topic_pir, pirState, retained, qos, dup);//publishing to Arduino 2 when PIR state is High 
     mqttClient.print(pirState);
-    mqttClient.endMessage();
+    mqttClient.endMessage(); //End of message
   }
 }
 void wifi_ble(){
   // attempt to connect to WiFi network:
   while (status != WL_CONNECTED) {
     Serial.print("Attempting to connect to WPA SSID: ");
-    Serial.println(ssid);
+    //Serial.println(ssid);
     // Connect to WPA/WPA2 network:
     status = WiFi.begin(ssid, pass);
-    // wait 10 seconds for connection:
+    // wait 5 seconds for connection:
     delay(5000);
   }
   // you're connected now, so print out the data:
   Serial.print("You're connected to the network ");
   printWifiData();
-  mqttClient.setUsernamePassword(Mqtt_User, Mqtt_Pass);
+  mqttClient.setUsernamePassword(Mqtt_User, Mqtt_Pass); //tries to connect to mqtt broker
   if (!mqttClient.connect(broker, port)) {
+    //if connection to broker was not success BLE is switched ON 
      if (!BLE.begin()) {
       Serial.println("starting Bluetooth® Low Energy module failed!");
       while (true);
     }
     BLE.setLocalName("PIR_status");   // set the local name peripheral advertises
     BLE.setAdvertisedService(pirService);   // set the UUID for the service this peripheral advertises:
-    pirService.addCharacteristic(pirCharacteristic);
-    BLE.addService(pirService);
+    pirService.addCharacteristic(pirCharacteristic); //adds the characteristic to service
+    BLE.addService(pirService); //add the service
     pirCharacteristic.writeValue(0);
-    BLE.advertise();
+    BLE.advertise(); //advertise the service which allows other BLE modules to connect
     Serial.print("BLE started");
-    ble_status = 1;
-    delay(5000);
+    digitalWrite(LED_BUILTIN, HIGH); //Builtin LED on Arduino is set to high if BLE is swithced on
+    ble_status = 1; //setting the status to 1
+    delay(5000); //waiting 5 sec for BLE setup and device coonection
   }
   else{
+    //connection to mqtt is successful
     Serial.println("You're connected to the MQTT broker!");
     Serial.println();
     Serial.print("Subscribing to topics: ");
     Serial.println();
-    mqttClient.onMessage(onMqttMessage);
-  // subscribe to a topic
+    mqttClient.onMessage(onMqttMessage); //callback function which is invoked when a message is recieved
     // subscribe to a topic
-    mqttClient.subscribe(topic,1);
+    mqttClient.subscribe(topic,1); 
     ble_status=0;
+    digitalWrite(LED_BUILTIN, LOW);
   }
 }
 void onMqttMessage(int messageSize){
@@ -143,13 +151,14 @@ void onMqttMessage(int messageSize){
       msg= msg+(char)mqttClient.read();
     }
     Serial.println(msg);
+    //Based on the message LED device status is changed
     if(msg == "ON") {
       digitalWrite(led_pin, HIGH);
       timer =0;
     } else {
       digitalWrite(led_pin, LOW);
     }
-    msg="";
+    msg=""; //clearing the message
 }
 void printWifiData() {
   // print your board's IP address:
--- Arduino_2/Arduino_2.ino
+++ Arduino_2/Arduino_2.ino
@@ -6,7 +6,7 @@
 #include "DHT.h"
 #define DHTPIN 2
 #define DHTTYPE DHT11
-DHT dht(DHTPIN, DHTTYPE);
+DHT dht(DHTPIN, DHTTYPE); //Pin connected to and type of DHT used
 
 #include "wifi_cred.h"
 
@@ -19,8 +19,8 @@ BLECharacteristic pirCharacteristic;
 char ssid[] = SECRET_SSID;        // your network SSID (name)
 char pass[] = SECRET_PASS;    // your network password (use for WPA, or use as key for WEP)
 int status = WL_IDLE_STATUS;     // the WiFi radio's status
-const int led_pin = 10;
-const char broker[] = "10.13.122.148"; // Address of the MQTT server
+const int led_pin = 10; //LED device on pin 10
+const char broker[] = "10.13.122.148"; // Address of the MQTT broker
 const int  port     = 1883;
 const char topic_status[] = "ToArduino/temp";
 const char topic_temp[] = "ToArduino/temp_set";
@@ -29,63 +29,67 @@ const char topic1[] = "ToHost/temp";
 String msg = "";
 int count =0;
 int led_state = 0;
-float set_temp = 27.0;
+float set_temp = 27.0;  //Threshold Temperature
 const long interval = 2000;
 const long mqtt_check = 10000*12;
 unsigned long previousMillis = 0;
 unsigned long previousMillis1 = 0;
+unsigned long previousMillis2 =0;
 int mqtt_status;
 int ble_status;
-const int subscribeQos = 1;
+const int subscribeQos = 1; //QoS of the MQTT subscriber
 
 void setup() {
   //Initialize serial and wait for port to open:
   Serial.begin(9600);
-  //while (!Serial) {
-    ; // wait for serial port to connect. Needed for native USB port only
-  //}
+  //Both the arduino are tested without connecting to PC so below lines are commented
+  /*
+  while (!Serial) {
+    wait for serial port to connect. Needed for native USB port only
+  }*/
   // check for the WiFi module:
   if (WiFi.status() == WL_NO_MODULE) {
     Serial.println("Communication with WiFi module failed!");
     // don't continue
     while (true);
   }
-  pinMode(led_pin, OUTPUT);
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(led_pin, OUTPUT); //LED device pin set as output
+  pinMode(LED_BUILTIN, OUTPUT); //Builtin led set as output
   digitalWrite(led_pin, LOW);
-  wifi_ble();
-  dht.begin();
+  wifi_ble(); //Function for Wifi or BLE
+  dht.begin(); //start the DHT service
 }
 
 void loop() {
-  float temp = dht.readTemperature();
-  float humidity = dht.readHumidity();
+  float temp = dht.readTemperature(); //Reading temperature from DHT11
+  float humidity = dht.readHumidity(); //Reading Humidity from DHT11
   unsigned long currentMillis = millis();
-  if (currentMillis - previousMillis >= interval) { 
-    previousMillis = currentMillis;
+  if (currentMillis - previousMillis1 >= interval) { //printing the values on serial monnitor at regular intervals
+    previousMillis1 = currentMillis;
     Serial.println(temp);
     Serial.println(humidity);
     //send_status(temp, humidity);
   }
   led_state = digitalRead(led_pin);
-  if (isnan(humidity) || isnan(temp)) {
+  if (isnan(humidity) || isnan(temp)) { //retuns if failed to read from the sensor
     Serial.println(F("Failed to read from DHT sensor!"));
     return;
   }
-  if (temp >= set_temp && led_state==HIGH) {
+  if (temp >= set_temp && led_state==HIGH) { //Based on the threshlod value the connected device will be switched OFF
     digitalWrite(led_pin, LOW);
     //Serial.print("Heater Switched OFF");
   }
-  if(mqttClient.connected())
+  if(mqttClient.connected()) //If MQTT client is connected below function is called
     mqtt_func(temp, humidity);
   else{
-    if(ble_status=1){
+    if(ble_status=1){ //if ble is switched on peripheral_ble() is called
       //Serial.println("Entered");
       peripheral_ble();
     }
     else{
-      //WiFi.disconnect();
-      wifi_ble();
+      WiFi.disconnect();
+      delay(5000);
+      wifi_ble(); //function called to try to make the MQTT connection again if not BLE 
     }
     //unsigned long currentMillis = millis();
     //if (currentMillis - previousMillis1 >= mqtt_check){ 
@@ -96,15 +100,16 @@ void loop() {
 }
 
 void mqtt_func(float temp, float humidity){
-  mqttClient.poll();
+  mqttClient.poll(); //TO keep alive the mqtt connection
   unsigned long currentMillis = millis();
   if (currentMillis - previousMillis >= interval) { 
     previousMillis = currentMillis;
-    send_status(temp, humidity);
+    send_status(temp, humidity); //Call the send_status function every 2 seconds
   }
 }
 void peripheral_ble(){
   BLEDevice peripheral = BLE.available();
+  //if a peripheral device is available below details are printed
   if (peripheral) {
     // discovered a peripheral, print out address, local name, and advertised service
     Serial.print("Found ");
@@ -116,40 +121,51 @@ void peripheral_ble(){
     Serial.println();
   //
     Serial.println(peripheral.localName());
-  if (peripheral.localName() != "PIR_status") {
+  if (peripheral.localName() != "PIR_status") { //compares the name of the pheripheral device
       return;
   }
-  BLE.stopScan();
-  if (peripheral.connect()) {
+  BLE.stopScan(); //stops scanning for devices
+  if (peripheral.connect()) { //try to connect to the peripeheral arduino device
     Serial.println("Connected");
   }
+  //checks for the attributes
   if (peripheral.discoverAttributes()) {
     Serial.println("Attributes discovered");
   }
   //pirCharacteristic = peripheral.characteristic("19B10001-E8F2-537E-4F6C-D104768A1214");
-  ble_func(peripheral);
+  ble_func(peripheral); //calls the function
   }
-  BLE.scanForUuid("19B10000-E8F2-537E-4F6C-D104768A1214");
+  BLE.scanForUuid("19B10000-E8F2-537E-4F6C-D104768A1214"); //if not connected to peripheral try to scan for BLE device
   delay(5000);
 }
 void ble_func(BLEDevice peripheral){
   Serial.println("Entered Ble func");
   //peripheral.connect();
-  while(peripheral.connected()){
-  BLE.poll();
-  pirCharacteristic = peripheral.characteristic("19B10001-E8F2-537E-4F6C-D104768A1214");
+  while(peripheral.connected()){ //while connected to the arduino device
+  BLE.poll();//poll to keep the connection alive
+  pirCharacteristic = peripheral.characteristic("19B10001-E8F2-537E-4F6C-D104768A1214"); //check for the pir characterisctic
   byte valued;
-  pirCharacteristic.readValue(valued);
-  Serial.println(valued);
-  led_state = digitalRead(led_pin);
+  pirCharacteristic.readValue(valued); //reads the characteristic value
+  //Serial.println(valued);
+  led_state = digitalRead(led_pin); //read the edn device status
   if (valued && (led_state == 0)){
     //Serial.println("entered");
-    digitalWrite(led_pin, HIGH);
+    digitalWrite(led_pin, HIGH); // switches ON if PIR characteristic value is high and device is OFF
   }
   float temp = dht.readTemperature();
+  float humidity = dht.readHumidity();
   if (temp >= set_temp && led_state==HIGH) {
     digitalWrite(led_pin, LOW);
     //Serial.print("Heater Switched OFF");
+  }
+  if (millis() - previousMillis2 >= 5*1000UL){
+    previousMillis2 = millis();
+    Serial.print("PIR satate of Arduino 1: ");
+    Serial.print(valued);
+    Serial.println("Temperature and Humidity Values: ");
+    Serial.print(temp);
+    Serial.print("C ");
+    Serial.print(humidity);
   }
   /*if (millis() - previousMillis1 >= 3*60*1000UL) 
   {
@@ -161,6 +177,7 @@ void ble_func(BLEDevice peripheral){
    wifi_ble();
   }*/
   }
+  //if the connection is lost BLE starts scanning and peripheral_ble() function is called
   if (!peripheral.connected()) {
     BLE.scanForUuid("19B10000-E8F2-537E-4F6C-D104768A1214");
     peripheral_ble();
@@ -174,11 +191,12 @@ void send_status(float temp, float humidity){
    tempObject["humidity"]=humidity;
    tempObject["stemp"]=set_temp;
    tempObject["status"]=digitalRead(led_pin);
+   //Assigning the values of current temperature, humidity, threshold temperature, and device status to objcet and converting that object to json string
    String jsonString = JSON.stringify(tempObject);
    Serial.println(jsonString);
-   mqttClient.beginMessage(topic1, jsonString.length(), retained, qos, dup);
-   mqttClient.print(jsonString);
-   mqttClient.endMessage();
+   mqttClient.beginMessage(topic1, jsonString.length(), retained, qos, dup); //Starting publishing to the topic with QoS 1
+   mqttClient.print(jsonString); //publishing the message
+   mqttClient.endMessage(); //end of message
 }
  
 void wifi_ble(){
@@ -195,27 +213,29 @@ void wifi_ble(){
   // you're connected now, so print out the data:
   Serial.print("You're connected to the network");
   printWifiData();
-  mqttClient.setUsernamePassword(Mqtt_User, Mqtt_Pass);
-  if (!mqttClient.connect(broker, port)) {
+  mqttClient.setUsernamePassword(Mqtt_User, Mqtt_Pass); //setting the username and password for making connection to broker
+  if (!mqttClient.connect(broker, port)) { //try to connect to MQTT broker
     //WiFi.disconnect();
+    //if connection failed wifi is stopped and BLE is started
     delay(2000);
     WiFi.end();
     status = WL_IDLE_STATUS;
-    delay(2000);
     Serial.print("MQTT connection failed! Error code = ");
     Serial.println(mqttClient.connectError());
-    BLE.begin();
-    digitalWrite(LED_BUILTIN, HIGH);
+    delay(2000);
+    BLE.begin(); //BLE is started
+    digitalWrite(LED_BUILTIN, HIGH); //Built in LED is set to high if BLE is started
     //BLE.setEventHandler(BLEDiscovered, ble_func);
-    Serial.println("Bluetooth® Low Energy Central - LED control");
-    BLE.scanForUuid("19B10000-E8F2-537E-4F6C-D104768A1214");
-    delay(5000);
+    Serial.println("Bluetooth® Low Energy Service started");
+    BLE.scanForUuid("19B10000-E8F2-537E-4F6C-D104768A1214");//BLE scans for the defined UUID
+    delay(5000); //delay for scanning and setting up
     //peripheral_ble();
     ble_status = 1;
     mqtt_status =0;
-    Serial.println("connected to uuid");
+    //Serial.println("Connected to UUID");
   }
   else{
+    //if mqtt connection was successful
     mqtt_status = 1;
     ble_status = 0;
     digitalWrite(LED_BUILTIN, LOW);
@@ -223,8 +243,8 @@ void wifi_ble(){
     Serial.println();
     Serial.print("Subscribing to topics: ");
     Serial.println();
-    mqttClient.onMessage(onMqttMessage);
-  // subscribe to a topic
+    mqttClient.onMessage(onMqttMessage); //callback function when a message is recieved
+    // subscribe to below topc with QoS 1
     mqttClient.subscribe(topic_status, subscribeQos);
     mqttClient.subscribe(topic_temp, subscribeQos);
     mqttClient.subscribe(topic_pir, subscribeQos);
@@ -243,17 +263,20 @@ void onMqttMessage(int messageSize){
     while (mqttClient.available()) {
       msg= msg+(char)mqttClient.read();
     }
-    if (recieved == topic_status){
+    //Device subscribes to three topics based on the topics decisions are made
+    if (recieved == topic_status){//topic to control the end device
       if(msg == "ON") {
         digitalWrite(led_pin, HIGH);
       }else {
         digitalWrite(led_pin, LOW);
       } 
     }
+    //topic to set the threshold temperature
     else if (recieved == topic_temp){
       set_temp = msg.toFloat();
       Serial.println(msg);
     }
+    //topic for Arduino 1 communication to set the device high  
     else if(recieved == topic_pir){
       if(msg == "1")
         digitalWrite(led_pin, HIGH);
